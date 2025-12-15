4ef880cf2e89ff9d9a4d7a54a0406281d3153c8d
Bailly Hugo <gucci60610@gmail.com>
2025-12-15 14:24:51 +0100

    fixing cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.1].define(version: 2025_12_15_102632) do
-=======
 ActiveRecord::Schema[7.1].define(version: 2025_12_15_120857) do
->>>>>>> aichat
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
